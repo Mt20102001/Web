2a1dd7fcfe8b0addb0240b25b82c7ad6a349a985
luonyeuvo2001@gmail.com <luonyeuvo2001@gmail.com>
2021-07-20 21:39:27 +0700

    new HTML

--- Model/ProductEditModel.cs
+++ Model/ProductEditModel.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace HTML_Form_Basic.Model
 {
     public class ProductEditModel
@@ -6,5 +13,8 @@ namespace HTML_Form_Basic.Model
         public string Name { get; set; }
         public decimal Rate { get; set; }
         public int Rating { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
